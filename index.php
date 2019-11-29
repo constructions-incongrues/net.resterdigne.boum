be7ffe38d05c33758fd7dc79d8e1e036df30d5e3
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-11-29 04:14:13 +0100

    hop

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ $parts = explode('/', $image);
 ?>
 <img id="victim" src="<?php echo $parts[6].'/'.$parts[7].'/'.$parts[8] ?>" style="width:100%; height:100%">
 <script>
+    window.setInterval(() => {
+        window.location.reload();
+    }, 3000);
     document.getElementById('victim').onclick = function(e) {
         e.target.src = 'explosion.gif';
         e.target.onclick = function() {
