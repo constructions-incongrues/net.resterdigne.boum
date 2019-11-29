6766b35d639b41de0054b0fc93417a4ee28c4c03
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-11-29 06:00:33 +0100

    Deploy

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
 <?php
 require_once(__DIR__.'/vendor/autoload.php');
+
 use Symfony\Component\Finder\Finder;
+
 $finder = new Finder();
 $finder->files()->in(__DIR__.'/downloads');
 $iterator = iterator_to_array($finder);
 $image = array_rand($iterator);
-$parts = explode('/', $image);
+$partsImage = explode('/', $image);
+
+$finder = new Finder();
+$finder->files()->in(__DIR__.'/downloads');
+$iterator = iterator_to_array($finder);
+$image = array_rand($iterator);
+$partsPunition = explode('/', $image);
 
 ?>
-<img id="victim" src="<?php echo $parts[6].'/'.$parts[7].'/'.$parts[8] ?>" style="width:100%; height:100%">
+
+<img src="<?php echo $parts[6].'/'.$parts[7].'/'.$parts[8] ?>" style="width:100%; height:100%">
+
 <script>
     window.setInterval(() => {
         window.location.reload();
-    }, 3000);
+    }, 1500);
     document.getElementById('victim').onclick = function(e) {
         if (e.target.src.match(/victim/)) {
             console.log('ASSASSIN !');
         } else {
             console.log('BRAVO !');
         }
-        e.target.src = 'explosion.gif';
+        e.target.src = '<?php echo $parts[6].'/'.$parts[7].'/'.$parts[8] ?>';
         e.target.onclick = function() {
             window.location.reload();
         }
