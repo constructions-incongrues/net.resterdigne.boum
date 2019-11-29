46427c9d3e99934d3de78050538629d0b13d342d
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-11-29 03:50:18 +0100

    bisous

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 require_once(__DIR__.'/vendor/autoload.php');
 use Symfony\Component\Finder\Finder;
 $finder = new Finder();
-$finder->depth('== 3')->files()->in(__DIR__.'/downloads');
-foreach ($finder as $file) {
-    var_dump($file->getRealPath());
-}
+$finder->files()->in(__DIR__.'/downloads');
+$iterator = iterator_to_array($finder);
+$image = array_rand($iterator);
+$parts = explode('/', $image);
 
 ?>
-<img id="victim" src="" style="width:100%; height:100%">
+<img id="victim" src="<?php echo $parts[6].'/'.$parts[7].'/'.$parts[8] ?>" style="width:100%; height:100%">
 <script>
-    document.getElementById('victim').src = '';
     document.getElementById('victim').onclick = function(e) {
         e.target.src = 'explosion.gif';
         e.target.onclick = function() {
