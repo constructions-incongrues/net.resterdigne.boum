e25fbf3adf5fbf54ecbc1e80f27806051c531097
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-12-03 05:24:07 +0100

    Insultes

--- src/public/index.php
+++ src/public/index.php
@@ -15,14 +15,23 @@ $partsImageActors = array_slice(explode('/', array_rand(iterator_to_array($finde
 $finderRetributions = new Finder();
 $finderRetributions->files()->in(__DIR__.'/assets/images/retribution');
 $partsImageRetributions = array_slice(explode('/', array_rand(iterator_to_array($finderRetributions))), -3, 3);
+
+// Insult
+$insults = file(__DIR__.'/../var/insults.txt');
+$insult = $insults[array_rand($insults)];
 ?>
 
 <title>Ma main invisible, tu la veux sur la gueule ? | v0.2.0</title>
 
 <body style="background-color:#000;overflow:hidden;">
+
 <div style="text-align:center;">
     <img id="image" src="assets/images/<?php echo rawurldecode(implode('/', $partsImageActors)) ?>" style="height:100%;">
 </div>
+
+<p id="overlay" style="display:none; color: #fff; font-size: 250px; text-shadow: 2px 2px 2px #000; text-align: center; position: absolute; top: 20%; left: 50%; transform: translate(-50%, -50%); width: 100%;">
+<?php echo $insult; ?>
+</p>
 
 <script>
     var isPaused = false;
@@ -39,6 +48,7 @@ $partsImageRetributions = array_slice(explode('/', array_rand(iterator_to_array(
             } else {
                 console.log('BRAVO !');
                 e.target.src = 'assets/images/<?php echo rawurldecode(implode('/', array_slice(explode('/', array_rand(iterator_to_array($finderRetributions))), -3, 3))) ?>';
+                document.getElementById('overlay').style.display = 'block';
             }
         });
     });
