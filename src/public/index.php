e70463ac79aa11a80763af3ad40010cb527b438e
Tristan Rivoallan <tristan.rivoallan-prestataire@laposte.fr>
2019-12-03 06:18:15 +0100

    Meilleur rythme

--- src/public/index.php
+++ src/public/index.php
@@ -7,7 +7,7 @@ use Symfony\Component\Finder\Finder;
 $finderActors = new Finder();
 $finderActors
     ->files()
-    ->in(__DIR__.'/assets/images/oppressed')
+    // ->in(__DIR__.'/assets/images/oppressed')
     ->in(__DIR__.'/assets/images/oppressors');
 $partsImageActors = array_slice(explode('/', array_rand(iterator_to_array($finderActors))), -3, 3);
 
@@ -21,36 +21,68 @@ $insults = file(__DIR__.'/../var/insults.txt');
 $insult = $insults[array_rand($insults)];
 ?>
 
-<title>Ma main invisible, tu la veux sur la gueule ? | v0.3.1</title>
+<html>
+    <head>
+        <title>Ma main invisible, tu la veux sur la gueule ? | v0.3.1</title>
+        <style>
+body {
+    background-color: #000;
+    overflow: hidden;
+    text-align: center;
+}
 
-<body style="background-color:#000;overflow:hidden;">
+#image {
+    height: 100%;
+}
 
-<div style="text-align:center;">
-    <img id="image" src="assets/images/<?php echo rawurldecode(implode('/', $partsImageActors)) ?>" style="height:100%;">
-</div>
+#overlay {
+    display:none;
+    color: #fff;
+    font-size: 250px;
+    text-shadow: 2px 2px 2px #000;
+    text-align: center;
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+}
+        </style>
+    </head>
 
-<p id="overlay" style="display:none; color: #fff; font-size: 250px; text-shadow: 2px 2px 2px #000; text-align: center; position: absolute; top: 20%; left: 50%; transform: translate(-50%, -50%); width: 100%;">
+    <body>
+
+        <img id="image" src="assets/images/<?php echo rawurldecode(implode('/', $partsImageActors)) ?>">
+
+        <p id="overlay">
 <?php echo $insult; ?>
-</p>
+        </p>
 
-<script>
-    var isPaused = false;
-    window.setInterval(() => {
-        if (!isPaused) {
-            window.location.reload();
-        }
-    }, 3000);
+        <script>
+            var isPaused = false;
+            window.setInterval(() => {
+                if (!isPaused) {
+                    window.location.reload();
+                }
+            }, 1000);
 
-    ['click','ontouchstart'].forEach(function(event) {
-        document.getElementById('image').addEventListener(event, function(e) {
-            if (e.target.src.match(/oppressed/)) {
-                console.log('ASSASSIN !');
-            } else {
-                console.log('BRAVO !');
-                e.target.src = 'assets/images/<?php echo rawurldecode(implode('/', array_slice(explode('/', array_rand(iterator_to_array($finderRetributions))), -3, 3))) ?>';
-                document.getElementById('overlay').style.display = 'block';
-            }
-        });
-    });
-</script>
-</body>
+            ['click','ontouchstart'].forEach(function(event) {
+                document.getElementById('image').addEventListener(event, function(e) {
+                    if (e.target.src.match(/oppressed/)) {
+                        console.log('ASSASSIN !');
+                    } else {
+                        isPaused = true;
+                        document.getElementById('overlay').style.display = 'block';
+                        window.setTimeout(() => {
+                            e.target.src = 'assets/images/<?php echo rawurldecode(implode('/', array_slice(explode('/', array_rand(iterator_to_array($finderRetributions))), -3, 3))) ?>';
+                            window.setTimeout(() => {
+                                isPaused = false;
+                            }, 2000);
+                        }, 1000);
+                        console.log('BRAVO !');
+                    }
+                });
+            });
+        </script>
+    </body>
+</html>
